6dcece32b3acfdafd2abd66b90e9a2ce2992ac7b
Bruce <graham.clodge@gmail.com>
2019-12-08 10:37:54 -0500

    Finished day 8 of AoC2019

---
+++ CGC.Advent.Core/Classes/SpaceImage.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using System.Drawing;
+
+namespace CGC.Advent.Core.Classes
+{
+    public class SpaceImage
+    {
+        public int Width { get; private set; } = int.MinValue;
+        public int Height { get; private set; } = int.MinValue;
+
+        public int[] Data { get; private set; } = null;
+        public List<int[]> Layers { get; private set; } = null;
+
+        public SpaceImage(string imageData, int width, int height)
+        {
+            //< Assign the pixel width/height
+            this.Width = width;
+            this.Height = height;
+            //< Parse the image data
+            this.Data = ParseData(imageData);
+            //< Get all the layers
+            GetLayers();
+        }
+
+        private static int[] ParseData(string imageData)
+        {
+            string data = null;
+            if (imageData.EndsWith(".txt"))
+            {
+                data = File.ReadAllText(imageData);
+            }
+            else
+            {
+                data = imageData;
+            }
+            return data.Select(c => int.Parse(c.ToString())).ToArray();
+        }
+
+        private void GetLayers()
+        {
+            //< Calculate the overall length of the layers
+            int layerLen = this.Width * this.Height;
+            //< Get the Layer container
+            this.Layers = new List<int[]>();
+            //< Iterate through all the pixels and build the layers
+            int idx = 0;
+            var currLayer = new List<int>();
+            while (idx < this.Data.Length)
+            {
+                //< Add to the layer until at capacity
+                if (currLayer.Count < layerLen)
+                {
+                    currLayer.Add(this.Data[idx]);
+                }
+                //< Make a new layer and add this field to it
+                else
+                {
+                    this.Layers.Add(currLayer.ToArray());
+                    currLayer = new List<int>();
+                    currLayer.Add(this.Data[idx]);
+                }
+
+                idx += 1;
+            }
+
+            //< Cleanup
+            if (currLayer.Count > 0)
+            {
+                this.Layers.Add(currLayer.ToArray());
+            }
+        }
+
+        public int[] GetLayerWithMinimumNumValues(int value)
+        {
+            int minIdx = -1;
+            int currCount = int.MaxValue;
+
+            //< Iterate over all layers, find the layer with the fewest of a given digit
+            for (int i = 0; i < this.Layers.Count; i++)
+            {
+                var count = this.Layers[i].Count(px => px == value);
+                if (count < currCount)
+                {
+                    minIdx = i;
+                    currCount = count;
+                }
+            }
+
+            //< Return that layer
+            return this.Layers[minIdx];
+        }
+
+        public static int GetNumDigits(int[] layer, int value)
+        {
+            return layer.Count(px => px == value);
+        }
+
+        public void GenerateFinalImage(string imageName)
+        {
+            var size = this.Width * this.Height;
+            var imageData = new int[size];
+
+            //< Iterate over each pixel in the final image
+            for (int i = 0; i < size; i++)
+            {
+                imageData[i] = GetFinalPixelValue(i);
+            }
+
+            //< Generate the image
+            var bmp = GenerateImage(imageData, this.Width, this.Height);
+            bmp.Save(imageName, System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        private int GetFinalPixelValue(int currIdx)
+        {
+            //< Iterate over each layer, return the first non-transparent value
+            for (int i = 0; i < this.Layers.Count; i++)
+            {
+                var layerValue = this.Layers[i][currIdx];
+                if (layerValue != 2)
+                {
+                    return layerValue;
+                }
+            }
+            //< If nothing non-transparent encountered, we're transparent mafk
+            return 2;
+        }
+
+        private static Bitmap GenerateImage(int[] image, int width, int height)
+        {
+            var bmp = new Bitmap(width, height);
+            for (int i = 0; i < image.Length; i++)
+            {
+                int x = i % width; //< Just need the left-over to see where we are in X
+                int y = i / width; //< Just need to find which row we're on
+
+                bmp.SetPixel(x, y, GetColor(image[i]));
+            }
+            return bmp;
+        }
+
+        private static Color GetColor(int value)
+        {
+            switch (value)
+            {
+                case 0:
+                    return Color.Black;
+                case 1:
+                    return Color.White;
+                default:
+                    return Color.Transparent;
+            }
+        }
+    }
+}
---
+++ CGC.Advent.Tests/DayEightTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace CGC.Advent.Tests
+{
+    [TestClass]
+    public class DayEightTests
+    {
+        //< I'm lazy right now, get at me
+        public static readonly string TestDir = @"D:\_dev_test\CGC.Advent";
+
+        //< From: https://adventofcode.com/2019/day/5#part2
+        private static List<Tuple<string, int, int>> KnownImages = new List<Tuple<string, int, int>>()
+        {
+            Tuple.Create("123456789012", 3, 2),
+        };
+
+        [TestMethod]
+        public void Test_KnownImage()
+        {
+            var imageData = "123456789012";
+            int width = 3;
+            int height = 2;
+
+            var img = new Core.Classes.SpaceImage(imageData, width, height);
+
+            Assert.IsTrue(img.Layers.Count == 2);
+            Assert.IsTrue(img.Layers.All(layer => layer.Length == 6));
+        }
+
+        const int Width = 25;
+        const int Height = 6;
+
+        [TestMethod]
+        public void Test_DayEight_PartOne()
+        {
+            var imageData = Path.Combine(TestDir, "Day8.Input.txt");
+            //< Parse the actual image data
+            var img = new Core.Classes.SpaceImage(imageData, Width, Height);
+            //< Get the layer with the fewest '0' digits
+            var minLayer = img.GetLayerWithMinimumNumValues(0);
+            //< Get the number of 1 digits and number of 2 digits, multiply 'em together
+            var countOnes = Core.Classes.SpaceImage.GetNumDigits(minLayer, 1);
+            var countTwos = Core.Classes.SpaceImage.GetNumDigits(minLayer, 2);
+            var res = countOnes * countTwos;
+
+            Assert.IsTrue(res == 1320);
+        }
+
+        [TestMethod]
+        public void Test_DayEight_PartTwo()
+        {
+            var imageData = Path.Combine(TestDir, "Day8.Input.txt");
+            //< Parse the actual image data
+            var img = new Core.Classes.SpaceImage(imageData, Width, Height);
+            //< Generate the final image
+            var imageName = Path.ChangeExtension(imageData, ".png");
+            img.GenerateFinalImage(imageName);
+
+            //< Can't really auto-parse and read the image.. it says 'RCYKR'
+            Assert.IsTrue(File.Exists(imageName));
+        }
+    }
+}
