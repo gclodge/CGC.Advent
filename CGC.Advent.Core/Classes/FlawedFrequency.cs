71ef65ec76f1174ab0bab0dfe0c15eed11ff3476
Bruce <graham.clodge@gmail.com>
2019-12-16 12:46:52 -0500

    Finished day 16 of AoC19

---
+++ CGC.Advent.Core/Classes/FlawedFrequency.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CGC.Advent.Core.Classes
+{
+    public class FlawedFrequency
+    {
+        private static readonly int[] _BasePattern = new int[] { 0, 1, 0, -1 };
+
+        public static string CalculatePartOne(string source, int phases)
+        {
+            var signal = ParseSignal(source);
+            for (int p = 0; p < phases; p++)
+            {
+                for (int j = 0; j < signal.Count; j++)
+                {
+                    int digit = 0;
+                    for (int k = 0; k < signal.Count; k++)
+                    {
+                        digit += signal[k] * GetMultiplier(j + 1, k);
+                    }
+
+                    signal[j] = Math.Abs(digit) % 10;
+                }
+            }
+
+            return string.Join("", signal.Take(8));
+        }
+
+        public static string CalculatePartTwo(string source, int phases)
+        {
+            var signal = ParseSignal(source);
+            int offset = GetOffset(source);
+            int times = (int)Math.Ceiling((double)(signal.Count * 10000 - offset) / (double)signal.Count);
+
+            var realSignal = Repeat(source, times); //< Need to slice this down
+            signal = ParseSignal(realSignal).Skip(offset % source.Length).ToList();
+            for (int p = 0; p < phases; p++)
+            {
+                for (int j = signal.Count - 2; j >= 0; j--)
+                {
+                    var digit = signal[j] + signal[j + 1];
+                    signal[j] = Math.Abs(digit) % 10;
+                }
+            }
+
+            return string.Join("", signal.Take(8));
+        }
+
+        private static int GetMultiplier(int num, int idx)
+        {
+            return _BasePattern[(int)Math.Floor(((double)idx + 1) % (4 * (double)num) / num)];
+        }
+
+        private static string Repeat(string source, int times)
+        {
+            var str = "";
+            for (int i = 0; i < times; i++)
+            {
+                str += source;
+            }
+            return str;
+        }
+
+        private static List<int> ParseSignal(string signal)
+        {
+            return signal.Select(c => int.Parse(c.ToString())).ToList();
+        }
+
+        public static int GetOffset(string signal)
+        {
+            return int.Parse(string.Join("", signal.Take(7)));
+        }
+    }
+}
---
+++ CGC.Advent.Tests/DailyTests/Day16Tests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using CGC.Advent.Core.Classes;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CGC.Advent.Tests
+{
+    [TestClass]
+    public class DaySixteenTests
+    {
+        private static readonly List<Tuple<string, int, string>> KnownSignals = new List<Tuple<string, int, string>>()
+        {
+            Tuple.Create("12345678", 4, "01029498"),
+            Tuple.Create("80871224585914546619083218645595", 100, "24176176"),
+            Tuple.Create("19617804207202209144916044189917", 100, "73745418"),
+            Tuple.Create("69317163492948606335995924319873", 100, "52432133"),
+        };
+
+        private static readonly List<Tuple<string, int, string>> KnownOffsetSignals = new List<Tuple<string, int, string>>()
+        {
+            Tuple.Create("03036732577212944063491565474664", 100, "84462026"),
+            Tuple.Create("02935109699940807407585447034323", 100, "78725270"),
+            Tuple.Create("03081770884921959731165446850517", 100, "53553731"),
+        };
+
+        [TestMethod]
+        public void Test_KnownSignals()
+        {
+            foreach (var testTup in KnownSignals)
+            {
+                var sig = FlawedFrequency.CalculatePartOne(testTup.Item1, testTup.Item2);
+                Assert.IsTrue(sig.StartsWith(testTup.Item3));
+            }
+        }
+
+        [TestMethod]
+        public void Test_DaySixteen_PartOne()
+        {
+            //< Get the input signal
+            var source = Path.Combine(TestHelper.TestDir, "Day16.Input.txt");
+            var signal = File.ReadAllText(source);
+            //< Parse/calculate the output
+            var sig = FlawedFrequency.CalculatePartOne(signal, 100);
+            Assert.IsTrue(sig == "25131128");
+        }
+
+        [TestMethod]
+        public void Test_KnownOffsetSignals()
+        {
+            foreach (var testTup in KnownOffsetSignals)
+            {
+                var sig = FlawedFrequency.CalculatePartTwo(testTup.Item1, 100);
+                Assert.IsTrue(sig == testTup.Item3);
+            }
+        }
+
+        [TestMethod]
+        public void Test_DaySixteen_PartTwo()
+        {
+            //< Get the input signal
+            var source = Path.Combine(TestHelper.TestDir, "Day16.Input.txt");
+            var signal = File.ReadAllText(source);
+            //< Parse/calculate the output
+            var sig = FlawedFrequency.CalculatePartTwo(signal, 100);
+
+            Assert.IsTrue(sig == "53201602");
+        }
+    }
+}
